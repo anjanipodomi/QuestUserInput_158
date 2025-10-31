2ec357f8313de68ccf9f943de08c01c224a6edc0
anjanipodomi <anjanidihapsari@gmail.com>
2025-10-31 09:26:11 +0700

    menambahkan OutlinedTextField untuk input nama dengan posisi di tengah secara horizontal

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,5 +1,5 @@
 package com.example.userinput
-
+import
 
 
 @Composable
@@ -20,6 +20,15 @@ fun FormDataDiri(modifier: Modifier
     Column(modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
 
+        horizontalAligment = Aligment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+
+
         )
+    }
 }
 
