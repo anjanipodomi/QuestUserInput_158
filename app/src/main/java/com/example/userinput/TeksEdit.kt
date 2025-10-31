3aa3eb8c47b7fe67b4dd9505a806a4eabd0b3c03
anjanipodomi <anjanidihapsari@gmail.com>
2025-10-31 09:27:29 +0700

    menambahkan label dan event onValueChange pada OutlinedTextField untuk input nama lengkap

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -27,7 +27,11 @@ fun FormDataDiri(modifier: Modifier
             shape = MaterialTheme.shapes.large,
             modifier = Modifier.width(250.dp),
 
+            label = {Text(text = "Nama Lengkap") },
 
+            onValueChange = {
+                textNama = it
+            }
         )
     }
 }
