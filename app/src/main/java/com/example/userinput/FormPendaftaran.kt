e546c02a6560a9448930204a901ed994a08e1809
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-01 19:55:50 +0700

    Menambahkan state untuk menyimpan hasil akhir input form (nama, alamat, jenis kelamin, dan status perkawinan)

--- app/src/main/java/com/example/userinput/FormPendaftaran.kt
+++ app/src/main/java/com/example/userinput/FormPendaftaran.kt
@@ -11,7 +11,10 @@ fun FormPendaftaran(modifier : Modifier = Modifier) {
     var textJK by remember { mutableStateOf("") }
     var textStatus by remember { mutableStateOf("") }
 
-
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
 
 
 
